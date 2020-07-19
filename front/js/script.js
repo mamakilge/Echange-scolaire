aef867a14baa823b33aa452815e985b4c64533bd
YOM-Gerard <67868664+YOM-Gerard@users.noreply.github.com>
2020-07-20 00:52:57 +0200

    JS

--- front/js/script.js
+++ front/js/script.js
@@ -1,15 +1,16 @@
 var echange = document.getElementById('echange');
 var echange2 = document.getElementById('echange2');
-var js = document.getElementsByClassName('js');
-var js2 = document.getElementsByClassName('js2');
+var js = document.getElementById('js');
+var js2 = " Ne recherche pas d'échange";
 console.log(echange2)
 console.log(echange)
 echange.addEventListener('click', function(){
     console.log("testclickechange")
     if(echange.style.color=='black'){
        echange.style.color=''
-       js2.style.display='block'       
+       js2.innerHTML = js;     
     }else{ echange.style.color='black'
+            js.innerHTML = js2;
 
 }
 
