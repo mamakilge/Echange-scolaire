1d77c195a5a86c525b316421cb5408e1a087694a
YOM-Gerard <67868664+YOM-Gerard@users.noreply.github.com>
2020-07-19 20:54:36 +0200

    Comme avant je crois

--- front/js/script.js
+++ front/js/script.js
@@ -1,1 +1,17 @@
-var a = 12;
+var echange = document.getElementById('echange');
+var echange2 = document.getElementById('echange2');
+var js = document.getElementsByClassName('js');
+var js2 = document.getElementsByClassName('js2');
+console.log(echange2)
+console.log(echange)
+echange.addEventListener('click', function(){
+    console.log("testclickechange")
+    if(echange.style.color=='black'){
+       echange.style.color=''
+       js2.style.display='block'       
+    }else{ echange.style.color='black'
+
+}
+
+})
+
