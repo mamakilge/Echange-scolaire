6a2053f1b8d44b1afed63f559883ff87b6e014cd
marisabonmati <68008846+marisabonmati@users.noreply.github.com>
2020-07-19 12:23:53 +0200

    Revert "Revert "Merge branch 'Annonce-prof' of https://github.com/Emma-Petreski/Mamakilge into Annonce-prof""
    This reverts commit b243e9455329c3721e6b0fa85fb6675215addfd1.

---
+++ front/script/script.js
@@ -0,0 +1,6 @@
+var echange = document.getElementById('echange');
+console.log(echange)
+echange.addEventListener('click', function(){
+    console.log("testclickechange")
+    echange.style.color='green'
+})
