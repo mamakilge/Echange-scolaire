69d348a5f728eb239f3f3dddb8c701a00be465b6
YOM-Gerard <67868664+YOM-Gerard@users.noreply.github.com>
2020-07-22 23:14:16 +0200

    Modal fini

--- front/js/script.js
+++ front/js/script.js
@@ -23,6 +23,9 @@ echange.addEventListener('click', function(){
 
 var modal = document.getElementById('modal');
 var btni = document.getElementById('btni');
+var btni1 = document.getElementById('btni1');
+var btni2 = document.getElementById('btni2');
+var btni3 = document.getElementById('btni3');
 
 function modalDisplay(){
 	modal.style.display= "none";
@@ -32,4 +35,22 @@ btni.onclick= function(){
 	modal.style.display= "block";
 }
 
+btni1.onclick= function(){
+	modal.style.display= "block";
+}
 
+btni2.onclick= function(){
+	modal.style.display= "block";
+}
+
+btni3.onclick= function(){
+	modal.style.display= "block";
+}
+
+btnNo.onclick= function(){
+	modal.style.display= "none";
+}
+
+btnNo2.onclick= function(){
+	modal.style.display= "none";
+}
