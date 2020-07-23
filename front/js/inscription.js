1587ba2034fa8df5eca6b027157d06c49f8714d7
YOM-Gerard <67868664+YOM-Gerard@users.noreply.github.com>
2020-07-23 11:44:53 +0200

    Inscription

---
+++ front/js/inscription.js
@@ -0,0 +1,19 @@
+// MODAL INSCRIPTIONvar modal = document.getElementById('modal');
+var btni = document.getElementById('btni');
+var btni1 = document.getElementById('btni1');
+var btni2 = document.getElementById('btni2');
+var btni3 = document.getElementById('btni3');function modalDisplay(){
+	modal.style.display= "none";
+}btni.onclick= function(){
+	modal.style.display= "block";
+}btni1.onclick= function(){
+	modal.style.display= "block";
+}btni2.onclick= function(){
+	modal.style.display= "block";
+}btni3.onclick= function(){
+	modal.style.display= "block";
+}btnNo.onclick= function(){
+	modal.style.display= "none";
+}btnNo2.onclick= function(){
+	modal.style.display= "none";
+}
