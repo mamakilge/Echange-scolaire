5ea036e903a7b0d880aac7c20e4660360ba34acb
Akil-p <67877158+Akil-p@users.noreply.github.com>
2020-07-27 13:13:03 +0200

    Version finale

--- front/js/script.js
+++ front/js/script.js
@@ -1,21 +1,3 @@
-// BOUTON ECHANGE EN JS
-var echange = document.getElementById('echange');
-var echange2 = document.getElementById('echange2');
-var js = document.getElementById('js');
-var js2 = " Ne recherche pas d'échange";
-var js3 = " Recherche d'échange"
-console.log(echange2)
-console.log(echange)
-echange.addEventListener('click', function(){
-    console.log("testclickechange")
-    if(echange.style.color=='black'){
-       echange.style.color=''
-       js.innerHTML = js3;     
-    }else{ echange.style.color='black'
-            js.innerHTML = js2;
-}
-})
-
 
   window.onload = function (){
     $('.bas').click(function(){
